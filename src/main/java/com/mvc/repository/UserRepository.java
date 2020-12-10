a25965a46b952385f4d17743704adfed84c33c50
buixuanvuong2410 <buixuanvuong2410@gmail.com>
2020-12-10 15:17:04 +0700

    ver1.1

--- src/main/java/com/mvc/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.mvc.repository;
-
-import com.mvc.entity.UserEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<UserEntity, Long> {
-    UserEntity findOneByUserNameAndStatus(String name, int status);
-    UserEntity findOneById(long id);
-}
